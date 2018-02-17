0c1e76cd761b379d08303c20d6db36eb47449e32
vladimir-trifonov <trifonov.dev@gmail.com>
2018-02-17 21:42:43 +0200

    Login component implemented

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from 'app/core/services'
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authService: AuthService, private _route: ActivatedRoute, private _router : Router) { }
 
   ngOnInit() {
+    const request_token = this._route.snapshot.queryParams['request_token']
+    if (request_token && !this._route.snapshot.queryParams['denied']) {
+      this._authService.getSession(request_token).subscribe(response => {
+        this._router.navigate(['/home'])
+      })
+    }
+  }
+
+  login() {
+    this._authService.getRequestToken().subscribe()
+
   }
 
 }
